074fbd0a6d53d93300bcc9cff6a057a01cbe1ad1
Peter Sturm <sturm@uni-trier.de>
2024-02-06 09:50:37 +0100

    Empty main

---
+++ src/org/ag_syssoft/sim4da.java
@@ -0,0 +1,8 @@
+package org.ag_syssoft;
+
+public class sim4da {
+    private static final String version = "sim4da V2.0";
+    public static void main(String[] args) {
+        System.out.println(version);
+    }
+}
